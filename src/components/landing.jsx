2cb10be02cf2651ee0ce4ae5c698d3c016150cd6
Nicole Laurin <43790599+nicolejlaurin@users.noreply.github.com>
2022-05-31 14:46:55 -0400

    components

---
+++ src/components/landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+	<div className='landing'>
+		<h1 className='name'>
+			Clints Company
+		</h1>
+		<div className='info'>
+			<h2>
+				We develop long lasting legacies for rising players,
+        supporting them every step along the way and influence
+        the sports industry for the better.
+			</h2>
+      <button class="button" onclick="">Get Started</button>
+		</div>
+	</div>
+);
